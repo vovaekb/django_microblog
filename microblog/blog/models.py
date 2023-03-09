45a5e4760c98533d238330ba2bce7abb92c57e0d
Vladimir Privalov <vovaprivalov@gmail.com>
2023-03-09 22:51:09 +0400

    Update Post model

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Post(models.Model):
-    # title = models.CharField(max_length=100)
-    text = models.TextField()
+    title = models.CharField(max_length=100)
+    text = models.TextField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Text: {self.text}"
