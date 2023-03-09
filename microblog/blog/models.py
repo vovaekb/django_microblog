34da4aba61a4d4a5c6b3f41b62de0321ca9c6a7a
Vladimir Privalov <vovaprivalov@gmail.com>
2023-03-09 22:51:09 +0400

    Update Post model

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
-    # title = models.CharField(max_length=100)
-    text = models.TextField()
+    title = models.CharField(max_length=100, null=True)
+    text = models.TextField(max_length=100)
+    created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Text: {self.text}"
